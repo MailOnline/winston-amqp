0f3e52befabed2126dd5153fa2639624b3fa5f42
Matthew W <matthew.woolf@mailonline.co.uk>
2015-01-20 13:13:25 +0000

    Initial implementation

---
+++ README.md
@@ -0,0 +1,63 @@
+Winston AMQP
+============
+
+A transport for Winston [https://www.npmjs.com/package/winston] which sends logging events to an AMQP-capable host, such as RabbitMQ.
+
+Installation
+------------
+
+	npm install winston
+	npm install winston-amqp
+	
+Usage
+-----
+
+	var winston = require('winston');
+
+	//
+	// Requiring `winston-amqp` will expose 
+	// `winston.transports.AMQP`
+	//
+	require('winston-amqp');
+	
+	winston.add(winston.transports.AMQP, options);
+	
+The AMQP transport takes the following options:
+
+* name:				The name of the logger. Defaults to the final path/script, e.g. "myapp/index.js". The name is included in the AMQP message
+* level:			The level of the logger
+* host:				The location of the AMQP server. See below for a description of the defaults and format of this value:
+* exchange:			The AMQP exchange name. This is optional is overrides the value specified by the 'host' option
+* routingKey:		The AMQP routingKey. This is optional is overrides the value specified by the 'host' option. A falsy value causes the routingKey to be set from the 'name' option.
+* exchangeOptions: 	Optional. As specified by 'connection.exchange' in [https://www.npmjs.com/package/amqp] 
+* publishOptions: 	Optional. As specified by 'exchange.publish' in [https://www.npmjs.com/package/amqp] 
+
+Host option
+-----------
+This can be either a URL object or a String. The format of the String (and URL properties) is:
+	
+				amqp://(user):(pass)@(host|ip):(port)/(exchangeName)/(routingKey)
+
+To simplify configuration (especially in clustered environments), the transport module first checks the 'host' option passed when the logger in created, then the environment variable WINSTON_AMQP and finally defaults to 'amqp://guest:guest@rabbit-logger:5672/winston/winston'.
+
+Events
+------
+winston-amqp emits an 'error' event if it cannot connection to the AMQP exchange. 
+
+
+Log Messages
+------------
+The messages posted to the AMQP server are in JSON format and consist of the following fields:
+
+	{
+	  "logHostName": "app.example.com",		// The 'hostname' of the app generating the logging request
+	  "timestamp": 1421752681391,			// The (local) time the log was generated
+	  "loggerName": "winston",				// The constant 'winston', to allow for filtering by whatever is using the data
+	  "name": "winston-amqp/test.js",		// The name of the logger, which defaults to the final 'path/script' of the node process generating the log
+	  "level": "info",						// The level of the log request
+	  "msg": "Log me",						// The logged message
+	  "meta": {								// Any meta-data in the log request
+	    "now": "2015-01-20T11:18:01.388Z"
+	  }
+	}
+
---
+++ index.js
@@ -0,0 +1,138 @@
+/*
+ * Winston logging transport for AMQP
+ */
+
+var util = require('util');
+var URL = require('url');
+var path = require('path');
+var amqp = require('amqp');
+
+var publish,buffer = [] ;
+
+var host = require('os').hostname ;
+
+function getProcessName() {
+	return require.main.filename.split(path.sep).slice(-2).join(path.sep) ;
+};
+
+var winston = require('winston');
+
+function assign(d,s) {
+	if (s)
+		Object.keys(s).forEach(function(k){
+			d[k] = s[k] ;
+		}) ;
+}
+
+/**
+ * Create a logger over AMQP
+ * 
+ * @param options
+ * 		name		String
+ * 		level		String
+ * 		host		URL/String amqp://user@pass:host:port/exchange/[routingkey]
+ * 		exchange	String
+ * 		exchangeOptions	
+ */ 
+var AMQP = winston.transports.AMQP = function (options) {
+	winston.Transport.call(this, options);
+	 
+	var self = this ;
+	var config = {
+			name:getProcessName(),
+			level:'debug',
+			host:process.env.WINSTON_AMQP || 'amqp://guest:guest@rabbit-logger:5672/winston/winston',
+			exchangeOptions:{
+				type: 'direct',
+				durable: true,
+				autoDelete: false
+			},
+			publishOptions:{
+				contentType: 'application/json',
+				deliveryMode: 2 	// Non-persistent (1) or persistent (2)
+			}
+	} ;
+
+	assign(config,options) ;
+
+	if (typeof config.host==='string')
+		config.host = URL.parse(config.host) ;
+	if (config.host.protocol!=='amqp:')
+		throw new Error("Incorrect protocol "+config.protocol) ;
+	if (!config.exchange)
+		config.exchange = config.host.pathname.split("/")[1] || 'winston';
+	if (!config.routingKey)
+		config.routingKey = config.host.pathname.split("/")[2] ;
+
+	// Name this logger
+	this.name = config.name ;
+	// Set the level from your options
+	this.level = config.level ;
+
+	// Configure your storage backing as you see fit
+	var connection = amqp.createConnection({
+		host: config.host.hostname,
+		port: config.host.port,
+		login: config.host.auth.split(":")[0],
+		password: config.host.auth.split(":")[1]
+	});
+
+	connection.on('ready', function () {
+		connection.exchange(config.exchange, config.exchangeOptions, function (exchange) {
+			publish = function(logMessage, callback){
+				var message = {
+						logHostName:host(),
+						timestamp:Date.now(),
+						loggerName:"winston",
+						name:self.name
+				};
+				assign(message,logMessage) ;
+
+				exchange.publish(config.routingKey || message.name, 
+						message, 
+						config.publishOptions, 
+						function(err){
+					callback && callback(err?new Error():null,!err) ;
+				});
+			};
+			// Now we're connected, play back any buffered log events
+			if (buffer) {
+				for (var i=0; i<buffer.length; i++) {
+					buffer[i].logger.log.apply(buffer[i].logger,buffer[i].args) ;
+				}
+				buffer = null ;
+			}
+		});
+	});
+
+	connection.on('error', function (err) {
+		self.emit('error',err) ;
+	});
+};
+
+
+//Inherit from `winston.Transport` so you can take advantage
+//of the base functionality and `.handleExceptions()`.
+
+util.inherits(AMQP, winston.Transport);
+
+AMQP.prototype.log = function (level, msg, meta, callback) {
+	// Store this message and metadata, maybe use some custom logic
+	// then callback indicating success.
+	if (typeof meta === 'function') {
+		callback = meta;
+		meta = {};
+	}
+	 
+	if (!publish) {
+		buffer.push({logger:this,args:[level,msg,meta]}) ;
+		callback && callback(null, true);
+	}
+	else {
+		publish({
+			level:level,
+			msg:msg,
+			meta:meta
+		},callback) ;
+	}
+};
